24e588609bb89c524762bc41e9609aad3edfeaad
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-10-13 20:38:57 -0400

    fix ordered list

--- assets/js/secondaryscript.js
+++ assets/js/secondaryscript.js
@@ -6,8 +6,10 @@ highScores = highScores.sort((a, b) => (a.score < b.score ? 1 : -1));
 
 // create a list item for each user
 for (var i = 0; i < highScores.length; i++) {
-  var userLi = (document.createElement("li").innerHTML =
-    highScores[i].name + " - " + highScores[i].score + "    ;    ");
-
-  scoreListEl.append(userLi);
+  var userLi = document.createElement("LI");
+  var userHTML = document.createTextNode(
+    highScores[i].name + " - " + highScores[i].score
+  );
+  userLi.appendChild(userHTML);
+  scoreListEl.appendChild(userLi);
 }
